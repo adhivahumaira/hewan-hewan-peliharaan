77b9722b490e7caa3c17e1a34420df4a7ce87861
adhivahumaira <adhivahumaira@gmail.com>
2026-02-12 13:51:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function ubahTeks() {
-    const teks = [
-        "Aku kucing paling gemay 😸",
-        "Aku kelinci super lucu 🐰",
-        "Aku panda gendut 🐼",
-        "Aku hamster imut 🐹"
-    ];
-
-    const random = Math.floor(Math.random() * teks.length);
-    document.getElementById("hasil").innerHTML = teks[random];
+function showInfo(message) {
+    document.getElementById("infoBox").innerHTML = message;
 }
